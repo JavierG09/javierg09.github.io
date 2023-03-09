257330949f359c63557d3a70f03b402262de7857
JavierG09 <127364183+JavierG09@users.noreply.github.com>
2023-03-08 22:50:34 -0300

    Initial commit

---
+++ CV/function.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log(Capturamos el evento click);
+    document.getElementById("tel").innerHTML = "Estamos probando nuestro primer evneto en JS"
+}
